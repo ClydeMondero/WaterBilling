c2712776ec6e0595a8eb054c6fa2b571bad489d4
ClydeMondero <92561235+ClydeMondero@users.noreply.github.com>
2023-04-21 17:44:47 +0800

    Updated Database File

--- Database.sql
+++ Database.sql
@@ -2,8 +2,8 @@ CREATE DATABASE Waterbillingsystem;
 
 USE Waterbillingsystem;
 
-CREATE TABLE Admin(id INTEGER PRIMARY KEY AUTO_INCREMENT, username VARCHAR(25), password VARCHAR(25));
+CREATE TABLE Administrator(id INTEGER PRIMARY KEY AUTO_INCREMENT, username VARCHAR(25), password VARCHAR(25));
 
-INSERT INTO Admin VALUES ('1', 'admin', 'admin');
+INSERT INTO Administrator VALUES ('1', 'admin', 'admin');
 
 SELECT * FROM Administrator;
