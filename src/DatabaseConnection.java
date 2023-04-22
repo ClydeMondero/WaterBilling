794ec5aad98eea1b72d4faee58e876c046444e4d
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-04-22 20:48:57 +0800

    Added Database SQL File

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.nio.file.*;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,14 +12,13 @@ public class DatabaseConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             
-            connect = DriverManager.getConnection("jdbc:mysql://localhost/waterbillingsystem", "root", "root");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/waterbilling", "root", "root");
             
-            System.out.println("Database Connected");
-        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connected");                                                                   
+            
+        } catch (ClassNotFoundException | SQLException ex) {                                
             Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } 
         
         return connect;  
     }
