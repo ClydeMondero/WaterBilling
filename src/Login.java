5af30141f661f20c901074494d48f5b7845f8008
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-04-25 20:16:58 +0800

    Fixed Login Bugs

--- src/Login.java
+++ src/Login.java
@@ -2,10 +2,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Login extends javax.swing.JFrame {        
@@ -43,11 +40,11 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        passwordTextField = new javax.swing.JTextField();
         usernameTextField = new javax.swing.JTextField();
         loginButton = new javax.swing.JButton();
-        signInLabel = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        loginLabel = new javax.swing.JLabel();
+        titleLabel = new javax.swing.JLabel();
+        passwordPasswordField = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -58,11 +55,11 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        signInLabel.setFont(new java.awt.Font("sansserif", 1, 24)); // NOI18N
-        signInLabel.setText("Log In");
+        loginLabel.setFont(new java.awt.Font("sansserif", 1, 24)); // NOI18N
+        loginLabel.setText("Log In");
 
-        jLabel1.setFont(new java.awt.Font("sansserif", 1, 30)); // NOI18N
-        jLabel1.setText("Water Billing System");
+        titleLabel.setFont(new java.awt.Font("sansserif", 1, 30)); // NOI18N
+        titleLabel.setText("Water Billing System");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -72,33 +69,38 @@ public class Login extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(155, 155, 155)
-                        .addComponent(signInLabel))
+                        .addComponent(loginLabel))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(45, 45, 45)
-                        .addComponent(jLabel1))
+                        .addComponent(titleLabel))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(76, 76, 76)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                             .addComponent(usernameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(passwordTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(passwordPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(59, Short.MAX_VALUE))
+                .addContainerGap(67, Short.MAX_VALUE))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {passwordPasswordField, usernameTextField});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(29, 29, 29)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
-                .addComponent(signInLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(titleLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addComponent(loginLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(48, 48, 48)
                 .addComponent(usernameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(39, 39, 39)
-                .addComponent(passwordTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(33, 33, 33)
+                .addGap(40, 40, 40)
+                .addComponent(passwordPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
                 .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(48, 48, 48))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {passwordPasswordField, usernameTextField});
 
         pack();
         setLocationRelativeTo(null);
@@ -110,16 +112,20 @@ public class Login extends javax.swing.JFrame {
         for (int i = 0; i < adminDatas.size(); i++) {
             if (usernameTextField.getText().equals(adminDatas.get(i).getUsername())) {
                 if (adminDatas.get(i).getStatus().equals("Active")) {
-                    if (passwordTextField.getText().equals(adminDatas.get(i).getPassword())) {                        
+                    if (passwordPasswordField.getText().equals(adminDatas.get(i).getPassword())) {                        
                         this.dispose();
                         JOptionPane.showMessageDialog(null, "Login Success!", "Login", JOptionPane.INFORMATION_MESSAGE);
                         new Admin().setVisible(true);
+                        break;
                     }else{
                         JOptionPane.showMessageDialog(null, "Login Failed!", "Login", JOptionPane.WARNING_MESSAGE);
                         usernameTextField.setText("");
-                        passwordTextField.setText("");
+                        passwordPasswordField.setText("");
                         System.out.println(++loginCounter);
                         if(loginCounter == 3){
+                            JOptionPane.showMessageDialog(null, "Account deactivated!", "Login", JOptionPane.ERROR_MESSAGE);
+                            usernameTextField.setText("");
+                            passwordPasswordField.setText("");
                             try {
                                 PreparedStatement statement = connect.prepareStatement("UPDATE Admin SET admin_status = ? WHERE admin_username = ?");
                                 statement.setString(1, "Deactivated");
@@ -134,13 +140,13 @@ public class Login extends javax.swing.JFrame {
                 } else {
                     JOptionPane.showMessageDialog(null, "Account deactivated!", "Login", JOptionPane.ERROR_MESSAGE);
                     usernameTextField.setText("");
-                    passwordTextField.setText("");
+                    passwordPasswordField.setText("");
                 }
 
             }            
         }
         for (int i = 0; i < staffDatas.size(); i++) {
-            if(usernameTextField.getText().equals(staffDatas.get(i).getUsername()) && passwordTextField.getText().equals(staffDatas.get(i).getPassword())){                
+            if(usernameTextField.getText().equals(staffDatas.get(i).getUsername()) && passwordPasswordField.getText().equals(staffDatas.get(i).getPassword())){                
                 this.dispose();
                 new Staff().setVisible(true);                
             }
@@ -149,10 +155,10 @@ public class Login extends javax.swing.JFrame {
       
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JButton loginButton;
-    private javax.swing.JTextField passwordTextField;
-    private javax.swing.JLabel signInLabel;
+    private javax.swing.JLabel loginLabel;
+    private javax.swing.JPasswordField passwordPasswordField;
+    private javax.swing.JLabel titleLabel;
     private javax.swing.JTextField usernameTextField;
     // End of variables declaration//GEN-END:variables
 }
