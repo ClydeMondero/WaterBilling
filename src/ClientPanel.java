b3c65e2820a4888be04762ebd398d8e3646b363f
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-05-01 16:28:58 +0800

    Updated Client Panel's Layout

--- src/ClientPanel.java
+++ src/ClientPanel.java
@@ -1,18 +1,109 @@
-class ClientPanel{
+class Client{
     private int id;
     private String lastName, firstName, middleName, address, phonNumber;
     private String rateclass;
     private double metersize;
     private int meterno;
     private String status;   
-    
-    
+
+    public Client(int id, String lastName, String firstName, String middleName, String address, String phonNumber, String rateclass, double metersize, int meterno, String status) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.address = address;
+        this.phonNumber = phonNumber;
+        this.rateclass = rateclass;
+        this.metersize = metersize;
+        this.meterno = meterno;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhonNumber() {
+        return phonNumber;
+    }
+
+    public void setPhonNumber(String phonNumber) {
+        this.phonNumber = phonNumber;
+    }
+
+    public String getRateclass() {
+        return rateclass;
+    }
+
+    public void setRateclass(String rateclass) {
+        this.rateclass = rateclass;
+    }
+
+    public double getMetersize() {
+        return metersize;
+    }
+
+    public void setMetersize(double metersize) {
+        this.metersize = metersize;
+    }
+
+    public int getMeterno() {
+        return meterno;
+    }
+
+    public void setMeterno(int meterno) {
+        this.meterno = meterno;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }       
 }
 
 
-public class Client extends javax.swing.JPanel {
+public class ClientPanel extends javax.swing.JPanel {
 
-    public Client() {
+    public ClientPanel() {
         initComponents();
     }
 
